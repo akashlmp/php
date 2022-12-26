f714e73d0d68351ec80e961956d06a43d30c8d95
Akash kuamr <akashkumarmaurya65@gmail.com>
2022-12-26 19:17:05 +0530

    introduction of function

---
+++ f1.php
@@ -0,0 +1,20 @@
+<?php
+function add($x,$y){
+    return ($x+$y);
+}
+// echo add(100,200);
+
+if (isset($_POST['calc'])) {
+    $n1= $_POST['n1'];
+    $n2= $_POST['n2'];
+    echo add($n1,$n2)."<br>";
+
+}
+?>
+<form method="post">
+    Enter First number :
+    <input type="number" name="n1"> <br><br>
+    Enter Second number :
+    <input type="number" name="n2"> <br><br>
+    <input type="submit" value="Submit" name="calc">
+</form>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- 
+    Function  : function is a named block of code which perform specific task. if you have a block of code which required at diffrent locations of program then you can create a function of that code and call it from desired loctions.
+
+    function in php is created by using function keyword followed by function name.
+
+    function functionName(parameters){
+        //code
+    
+    we can create function in four ways as per our requirement :-
+    1. No parameters and no return values.
+    2. No return types but parameters.
+    3. with parameters and with return type.
+    4. NO parameters but return type .
+    
+ -->
+<!-- Write a function to find sum of two numbers  -->
+
+<?php
+function add($x,$y){
+    return ($x+$y);
+}
+echo add(10,20)."<br>";
+echo add(100,200);
+?>
