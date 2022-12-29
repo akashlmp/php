2f8b5bca12be0f4897f9bf486833d13504028b4a
Akash kuamr <akashkumarmaurya65@gmail.com>
2022-12-29 22:38:50 +0530

    array

---
+++ array/demo1.php
@@ -0,0 +1,16 @@
+<!-- 
+    Array : Array are collection of data items stored under a single name.
+    array provide a mechanism for declaring and accessing several data 
+    items with only one identifier, thereby simplifying the task of
+    data management.
+    we use array when have to deal with multiple data items.
+    Numeric / Indexed Array - In this array index will be represented by
+    number. by default numeric array start from 0.
+    Associative Array - in this Array index/key will be represented
+    by a string .
+    Ex. $fee['Rahul'] = 500;
+    Multidimensional Array : array of array.
+ -->
+<?php
+
+?>
